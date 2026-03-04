fix: add normal gradient (v0.2)

threedgrt_tracer/tracer.py
@@ -22,7 +22,6 @@ import torch.utils.cpp_extension
 
 from threedgrut.datasets.protocols import Batch
 from threedgrut.utils.timer import CudaTimer
-from threedgrut_playground.utils.misc import color_normal
 
 logger = logging.getLogger(__name__)
 
@@ -251,7 +250,7 @@ class Tracer:
             "pred_rgb": pred_rgb,
             "pred_opacity": pred_opacity,
             "pred_dist": pred_dist,
-            "pred_normals": color_normal(pred_normals),
+            "pred_normals": pred_normals,
             "hits_count": hits_count,
             "frame_time_ms": (
                 self.frame_timer.timing() if self.frame_timer is not None else 0.0

threedgrut/datasets/dataset_colmap.py
@@ -244,8 +244,6 @@ class ColmapDataset(Dataset, BoundedMultiViewDataset, DatasetVisualization):
         self.poses = []
         self.image_paths = []
         self.mask_paths = []
-        if self.enable_normals:
-            self.normal_paths = []
 
         cam_centers = []
         for extr in logger.track(
@@ -275,7 +273,6 @@ class ColmapDataset(Dataset, BoundedMultiViewDataset, DatasetVisualization):
             # get normal
             normal_path = os.path.join(self.path, "normal", extr.name)
             normal_path = os.path.splitext(normal_path)[0]
-            self.normal_paths.append(normal_path)
 
             if not self.generate_normal:
                 continue
@@ -370,9 +367,6 @@ class ColmapDataset(Dataset, BoundedMultiViewDataset, DatasetVisualization):
     def __getitem__(self, idx) -> dict:
         # Load image and get its actual dimensions
         image_data = np.asarray(Image.open(self.image_paths[idx]))
-        normal_data = np.asarray(
-            self.normal_predictor.load_normal(self.normal_paths[idx])
-        )
         actual_h, actual_w = image_data.shape[:2]
 
         # Use actual image dimensions for output shape
@@ -382,10 +376,16 @@ class ColmapDataset(Dataset, BoundedMultiViewDataset, DatasetVisualization):
 
         output_dict = {
             "data": torch.tensor(image_data).unsqueeze(0),
-            "normal": torch.tensor(normal_data).unsqueeze(0),
             "pose": torch.tensor(self.poses[idx]).unsqueeze(0),
             "intr": self.get_intrinsics_idx(idx),
         }
+
+        if self.enable_normals:
+            normal_path = os.path.join(
+                self.path, "normal", self.image_paths[idx].split(".")[-2].split("/")[-1]
+            )
+            normal_data = np.asarray(self.normal_predictor.load_normal(normal_path))
+            output_dict["normal"] = torch.tensor(normal_data).unsqueeze(0)
 
         # Only add mask to dictionary if it exists
         if os.path.exists(mask_path := self.mask_paths[idx]):
@@ -400,7 +400,6 @@ class ColmapDataset(Dataset, BoundedMultiViewDataset, DatasetVisualization):
         data = batch["data"][0].to(self.device, non_blocking=True) / 255.0
         pose = batch["pose"][0].to(self.device, non_blocking=True)
         intr = batch["intr"][0].item()
-        normal = batch["normal"][0].to(self.device, non_blocking=True)
 
         assert data.dtype == torch.float32
         assert pose.dtype == torch.float32
@@ -412,12 +411,14 @@ class ColmapDataset(Dataset, BoundedMultiViewDataset, DatasetVisualization):
 
         sample = {
             "rgb_gt": data,
-            "normal_gt": normal,
             "rays_ori": rays_ori,
             "rays_dir": rays_dir,
             "T_to_world": pose,
             f"intrinsics_{camera_name}": camera_params_dict,
         }
+
+        if self.enable_normals:
+            sample["normal_gt"] = batch["normal"][0].to(self.device, non_blocking=True)
 
         if "mask" in batch:
             mask = batch["mask"][0].to(self.device, non_blocking=True) / 255.0

threedgrut/trainer.py
@@ -18,6 +18,7 @@ import time
 from collections import defaultdict
 from pathlib import Path
 from typing import Any, Optional, Union
+import wandb
 
 import numpy as np
 import torch
@@ -468,19 +469,27 @@ class Trainer3DGRUT:
 
         loss_normal = torch.zeros(1, device=self.device)
         lambda_normal = 0
-        if self.conf.render.enable_normals:
+        if (
+            self.conf.render.enable_normals
+            and self.global_step > self.conf.normal_loss_start_iteration
+        ):
             with torch.cuda.nvtx.range("loss-normal"):
-                # normal_pred = torch.nn.functional.normalize(normal_pred, dim=-1)
-                # normal_gt = torch.nn.functional.normalize(normal_gt, dim=-1)
-
+                normal_pred = torch.nn.functional.normalize(normal_pred, dim=-1)
+                normal_gt = torch.nn.functional.normalize(normal_gt, dim=-1)
+                mask = (normal_gt.norm(dim=-1) > 0) & (normal_pred.norm(dim=-1) > 0)
                 # cos loss
-                # loss_normal = 1 - (normal_pred * normal_gt).sum(dim=-1)
-                # loss_normal = loss_normal.mean()
+
+                cos = (normal_pred[mask] * normal_gt[mask]).sum(dim=-1)
+                # print(cos.mean())
+                loss_normal = 1 - cos
+                loss_normal = loss_normal.mean()
 
                 # l2 loss
-                loss_normal = ((normal_gt - normal_pred) ** 2).mean()
+                # loss_normal = ((normal_gt - normal_pred) ** 2).mean()
 
-            lambda_normal = self.conf.normal_loss_lambda
+                lambda_normal = self.conf.normal_loss_lambda
+                lambda_l1 -= lambda_normal
+            # print(lambda_normal * loss_normal)
 
         # Total loss
         loss = (
@@ -488,7 +497,7 @@ class Trainer3DGRUT:
             + lambda_ssim * loss_ssim
             + lambda_opacity * loss_opacity
             + lambda_scale * loss_scale
-            + loss_normal * lambda_normal
+            + lambda_normal * loss_normal
         )
         return dict(
             total_loss=loss,
@@ -602,49 +611,84 @@ class Trainer3DGRUT:
 
         if self.conf.enable_writer and global_step > 0 and global_step % self.conf.log_frequency == 0:
             loss = np.mean(batch_metrics["losses"]["total_loss"])
+
+            log_dict = {
+                "loss/total/train": loss,
+                "num_particles/train": self.model.num_gaussians,
+                "train/num_GS": self.model.num_gaussians,
+            }
+
             writer.add_scalar("loss/total/train", loss, global_step)
+
             if self.conf.loss.use_l1:
                 l1_loss = np.mean(batch_metrics["losses"]["l1_loss"])
                 writer.add_scalar("loss/l1/train", l1_loss, global_step)
+                log_dict["loss/l1/train"] = l1_loss
             if self.conf.loss.use_l2:
                 l2_loss = np.mean(batch_metrics["losses"]["l2_loss"])
                 writer.add_scalar("loss/l2/train", l2_loss, global_step)
+                log_dict["loss/l2/train"] = l2_loss
             if self.conf.loss.use_ssim:
                 ssim_loss = np.mean(batch_metrics["losses"]["ssim_loss"])
                 writer.add_scalar("loss/ssim/train", ssim_loss, global_step)
+                log_dict["loss/ssim/train"] = ssim_loss
             if self.conf.loss.use_opacity:
                 opacity_loss = np.mean(batch_metrics["losses"]["opacity_loss"])
                 writer.add_scalar("loss/opacity/train", opacity_loss, global_step)
+                log_dict["loss/opacity/train"] = opacity_loss
             if self.conf.loss.use_scale:
                 scale_loss = np.mean(batch_metrics["losses"]["scale_loss"])
                 writer.add_scalar("loss/scale/train", scale_loss, global_step)
+                log_dict["loss/scale/train"] = scale_loss
             if self.conf.render.enable_normals:
                 normal_loss = np.mean(batch_metrics["losses"]["normal_loss"])
                 writer.add_scalar("loss/normal/train", normal_loss, global_step)
+                log_dict["loss/normal/train"] = normal_loss
             if "psnr" in batch_metrics:
-                writer.add_scalar("psnr/train", batch_metrics["psnr"], self.global_step)
+                writer.add_scalar("psnr/train", batch_metrics["psnr"], global_step)
+                log_dict["psnr/train"] = batch_metrics["psnr"]
             if "ssim" in batch_metrics:
-                writer.add_scalar("ssim/train", batch_metrics["ssim"], self.global_step)
+                writer.add_scalar("ssim/train", batch_metrics["ssim"], global_step)
+                log_dict["ssim/train"] = batch_metrics["ssim"]
             if "lpips" in batch_metrics:
-                writer.add_scalar("lpips/train", batch_metrics["lpips"], self.global_step)
+                writer.add_scalar("lpips/train", batch_metrics["lpips"], global_step)
+                log_dict["lpips/train"] = batch_metrics["lpips"]
             if "hits_mean" in batch_metrics:
-                writer.add_scalar("hits/mean/train", batch_metrics["hits_mean"], self.global_step)
+                writer.add_scalar(
+                    "hits/mean/train", batch_metrics["hits_mean"], global_step
+                )
+                log_dict["hits/mean/train"] = batch_metrics["hits_mean"]
             if "hits_std" in batch_metrics:
-                writer.add_scalar("hits/std/train", batch_metrics["hits_std"], self.global_step)
+                writer.add_scalar(
+                    "hits/std/train", batch_metrics["hits_std"], global_step
+                )
+                log_dict["hits/std/train"] = batch_metrics["hits_std"]
             if "hits_min" in batch_metrics:
-                writer.add_scalar("hits/min/train", batch_metrics["hits_min"], self.global_step)
+                writer.add_scalar(
+                    "hits/min/train", batch_metrics["hits_min"], global_step
+                )
+                log_dict["hits/min/train"] = batch_metrics["hits_min"]
             if "hits_max" in batch_metrics:
-                writer.add_scalar("hits/max/train", batch_metrics["hits_max"], self.global_step)
-
+                writer.add_scalar(
+                    "hits/max/train", batch_metrics["hits_max"], global_step
+                )
+                log_dict["hits/max/train"] = batch_metrics["hits_max"]
             if "timings" in batch_metrics:
                 for time_key in batch_metrics["timings"]:
                     writer.add_scalar(
-                        "time/" + time_key + "/train", batch_metrics["timings"][time_key], self.global_step
+                        "time/" + time_key + "/train",
+                        batch_metrics["timings"][time_key],
+                        global_step,
                     )
+                    timing = batch_metrics["timings"][time_key]
+                    log_dict[f"time/{time_key}/train"] = timing
 
-            writer.add_scalar("num_particles/train", self.model.num_gaussians, self.global_step)
-            writer.add_scalar("train/num_GS", self.model.num_gaussians, self.global_step)
+            writer.add_scalar(
+                "num_particles/train", self.model.num_gaussians, global_step
+            )
+            writer.add_scalar("train/num_GS", self.model.num_gaussians, global_step)
 
+            wandb.log(log_dict, step=global_step)
             # # NOTE: hack to easily compare with 3DGS
             # writer.add_scalar("train_loss_patches/total_loss", loss, global_step)
             # writer.add_scalar("gaussians/count", self.model.num_gaussians, self.global_step)

threedgrut/utils/gui.py
@@ -26,6 +26,7 @@ from threedgrut.datasets.utils import DEFAULT_DEVICE, fov2focal
 from threedgrut.utils.logger import logger
 from threedgrut.utils.misc import to_np
 from threedgrut.utils.timer import CudaTimer
+from threedgrut_playground.utils.misc import color_normal
 
 trajectory = []
 
@@ -319,7 +320,7 @@ class GUI:
 
         elif style == "normals":
             # scale in rendering space
-            sple_onrm = 0.5 * (sple_onrm + 1)
+            sple_onrm = color_normal(sple_onrm)
             # append 1s for alpha
             sple_onrm = torch.cat((sple_onrm, torch.ones_like(sple_onrm[:, :, :, 0:1])), dim=-1)
             if self.update_from_device:

threedgrut_playground/engine.py
@@ -48,6 +48,8 @@ from threedgrut_playground.utils.mesh_io import (
 from threedgrut_playground.utils.spp import SPP
 from threedgrut_playground.utils.video_out import VideoRecorder
 from threedgrut_playground.utils.misc import visualize_depth_t_minmax_rgb
+from threedgrut_playground.utils.misc import color_normal
+
 
 #################################
 ##       --- Common ---        ##
@@ -899,13 +901,13 @@ class Engine3DGRUT:
             else:
                 dof_rb = self._render_playground_hybrid(dof_rays_ori, dof_rays_dir)
             if self.render_option == RenderOptions.RADIANCE:
-                keyStr = "pred_rgb"
+                next_frame = dof_rb["pred_rgb"]
             elif self.render_option == RenderOptions.DEPTH:
-                keyStr = "pred_dist"
+                next_frame = dof_rb["pred_dist"]
             else:
-                keyStr = "pred_normals"
+                next_frame = color_normal(dof_rb["pred_normals"])
             rb["rgb"] = self._accumulate_to_buffer(
-                rb["rgb"], dof_rb[keyStr], i, self.gamma_correction
+                rb["rgb"], next_frame, i, self.gamma_correction
             )
             rb["opacity"] = (rb["opacity"] * i + dof_rb["pred_opacity"]) / (i + 1)
 
@@ -927,12 +929,12 @@ class Engine3DGRUT:
                 spp_rb = self._render_playground_hybrid(rays.rays_ori, rays.rays_dir)
 
             if self.render_option == RenderOptions.RADIANCE:
-                keyStr = "pred_rgb"
+                buffer = spp_rb["pred_rgb"]
             elif self.render_option == RenderOptions.DEPTH:
-                keyStr = "pred_dist"
+                buffer = spp_rb["pred_dist"]
             else:
-                keyStr = "pred_normals"
-            batch = spp_rb[keyStr].sum(dim=0).unsqueeze(0)
+                buffer = color_normal(spp_rb["pred_normals"])
+            batch = buffer.sum(dim=0).unsqueeze(0)
 
             # if (self.render_option == RenderOptions.DEPTH):
             #     batch = visualize_depth_t_minmax_rgb(batch)

threedgrut_playground/tracer.py
@@ -256,7 +256,7 @@ class Tracer:
             "pred_rgb": pred_rgb,
             "pred_opacity": pred_opacity,
             "pred_dist": pred_dist,
-            "pred_normals": torch.nn.functional.normalize(pred_normals, dim=3),
+            "pred_normals": pred_normals,
             "hits_count": hits_count,
             "last_ray_o": ray_o,  # Rewritten by tracer
             "last_ray_d": ray_d,  # Rewritten by tracer